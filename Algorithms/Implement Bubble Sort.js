ad276b4379d0fffdc9ad5f38b49ab07d5cb8e959
Rakesh Seervi <rakeshbseervi16@gmail.com>
2021-04-21 18:47:57 +0530

    Create Implement Bubble Sort.js

---
+++ Algorithms/Implement Bubble Sort.js
@@ -0,0 +1,13 @@
+function bubbleSort(array) {
+  for(let i=0; i<array.length-1; i++)
+    for(let j=0; j<array.length-1-i; j++)
+      if(array[j]>array[j+1]){
+        array[j+1]+=array[j];
+        array[j] = array[j+1]-array[j];
+        array[j+1] = array[j+1]-array[j];
+      }
+      
+  return array;
+}
+
+bubbleSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
